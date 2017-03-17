5d322d6ee81b89ced351692e7ca349cee17a2806
Alitza <alitza-konata.13@hotmail.com>
2017-03-17 11:59:36 -0500

    crear el XML

--- proyectoIntegrador_Redes/src/Main.java
+++ proyectoIntegrador_Redes/src/Main.java
@@ -1,10 +1,14 @@
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import processing.core.PApplet;
+import processing.data.XML;
 
+@SuppressWarnings("serial")
 public class Main extends PApplet {
 	PApplet app;
 	Logica log;
+	
+	XML pasteleria;
 
 	public void settings() {
 		size(900, 581);
@@ -14,7 +18,57 @@ public class Main extends PApplet {
 		background(255);
 		app = this;
 		log = new Logica(this);
+		
+		pasteleria = loadXML("../data/pasteleria.xml");
+		XML nuevoPastel = parseXML("<user></user>");
+		nuevoPastel.setString("id", "nombre");
+		nuevoPastel.setString("correo", "password");
+		XML nuevoProducto = parseXML("<producto></producto>");
+		nuevoPastel.setString("nombre", "precio");
+		XML categoria = parseXML("<categoria>Pasteles</categoria>");
+		XML nuevaCompra = parseXML("<compra></compra>");
+		nuevoPastel.setString("user", "id");
+		XML nuevosProductos = parseXML("<productos></prodctos>");
+		nuevoPastel.setString("cant", "id");
+
+		nuevoProducto.addChild(categoria);
+		pasteleria.addChild(nuevoProducto);	
+		saveXML(pasteleria, "../data/pasteleria.xml");
+		
 	}
+
+	private void creandoXML(){
+		pasteleria = parseXML("<usuarios></usuarios>");
+		XML user = parseXML("<user></user>");
+		XML categoria = parseXML("<categoria>Pasteles</categoria>");
+		XML categoriaB = parseXML("<categoria>Caramelos</categoria>");
+
+		user.setString("id", "nombre");
+		user.setString("correo", "password");
+		user.addChild(categoria);
+		
+		pasteleria.addChild(categoria);
+		pasteleria.addChild(categoriaB);
+
+		XML[] hijosCategoria = pasteleria.getChildren();
+
+		for (int i = 0; i < hijosCategoria.length; i++) {
+			System.out.println(hijosCategoria[i].toString());
+			
+			XML[] hijosDeCadaCategoria = hijosCategoria[i].getChildren();
+			for (int j = 0; j < hijosDeCadaCategoria.length; j++) {
+				System.out.println(hijosDeCadaCategoria[j].toString());	
+			}
+		}
+
+		
+	}
+
+
+
+
+		
+		
 
 	public void draw() {
 		log.pintar();
